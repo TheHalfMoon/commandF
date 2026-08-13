ca791af29eea5c318833b22b9119cda557f976d4
Abdulaziz M. Shehri <alshehriofficial@gmail.com>
2026-08-14 02:39:54 +0300

    refactor(cf-02): fold inspection into package layer

--- crates/commandf-artifact/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod error;
-mod inspect;
-mod model;
-
-pub use error::ArtifactError;
-pub use inspect::inspect_package;
-pub use model::{ElementAddress, ElementView, PackageInspection, ResourceArtifact};
